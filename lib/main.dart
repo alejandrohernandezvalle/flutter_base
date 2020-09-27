52bd9717029ba229e65985f31d1c824329ffec7d
Alejandro Hernandez Valle <alejandrohernandezvalle@gmail.com>
2020-09-27 12:13:13 -0500

    Flutter Base

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Iniciando Flutter',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text('Hola Mundo'),
+        ),
+      ),
+    );
+  }
+}
